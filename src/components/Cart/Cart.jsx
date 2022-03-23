8f7e0f38718fa919e6c04765fbf080b36fe7f8a5
SShipon <nxshipon@gmail.com>
2022-03-24 01:03:01 +0630

    herrr

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
-
-const Cart = ({cart}) => {
+import './Cart.css'
+const Cart = (props) => {
+    const { cart } = props;
+    
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        total = total + product.price
+        shipping = shipping + product.shipping;
+    }
+    const txt = parseFloat((total * 0.1).toFixed(2))
+  const grandTotal = total + shipping + txt;
     return (
-      <div>
-        <h2>Order Summary:</h2>
-       <h6>select Items : {cart.length}</h6> 
+      <div className="cart">
+        <h6>Order Summary:</h6>
+        <h6>select Items : {cart.length}</h6>
+        <p>Total price: ${total} </p>
+        <p>Total Shipping:${shipping} </p>
+        <p>Text:{txt} </p>
+        <h3>Grand Total:{grandTotal.toFixed(2)} </h3>
       </div>
     );
 };
